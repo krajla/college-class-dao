79da3072a75705c7236ee46b66a54da0d3dd7943
Robert Krajla <rkrajla@yahoo.com>
2022-04-10 18:41:58 +0300

    Diploma NFT for graduates

--- contracts/ClassDao.sol
+++ contracts/ClassDao.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.7;
 
 import "hardhat/console.sol";
-import "./openzeppelin/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./TestFactory.sol";
 import "./Test.sol";
 
---
+++ test/diploma-contract-test.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Diploma", function () {
+    it("Should create Diploma NFT and not be able to transfer", async function () {
+        const DiplomaNft = await ethers.getContractFactory("DiplomaNft");
+        const diplomaNft = await DiplomaNft.deploy();
+        await diplomaNft.deployed();
+
+        const tokenUri = "http://example.com"
+        const [owner, other] = await ethers.getSigners();
+        const trx = await diplomaNft.awardDiploma(other.address, tokenUri);
+        await trx.wait();
+
+        expect(await diplomaNft.supportsInterface(0x80ac58cd)).to.equal(true);
+        expect(await diplomaNft.ownerOf(0x1)).to.equal(other.address);
+        expect(await diplomaNft.balanceOf(other.address)).to.equal(1);
+        await expect(diplomaNft.connect(other).transferFrom(other.address, owner.address, 0x1)).to.be.revertedWith("DiplomaNft: token can only be created for a recipient, and can not be transferred or burned");
+    });
+});
